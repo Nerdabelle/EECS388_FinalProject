a3cc3d1203776adae1ce996df245458fa970b19b
jsingh2412 <60803017+jsingh2412@users.noreply.github.com>
2023-04-26 17:56:05 -0500

    upload program and milestone2.csv

---
+++ prog.py
@@ -0,0 +1,17 @@
+import serial
+import csv
+import time
+
+ser = serial.Serial('/dev/ttyAMA1', 115200, timeout = 1)
+
+with open('milestone2.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+        angle = str(row[0])
+        speed = str(row[1])
+        duration = str(row[2])
+        command_str = "angle: " + angle + "speed: " + speed + "duration: " + duration
+        ser.write(bytes(command_str, 'utf-8'))
+        time.sleep(1)
+
+ser.close()
