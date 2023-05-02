71431e4ec6f97cf5dfa54cf6b9e4290af8ef579f
jsingh2412 <60803017+jsingh2412@users.noreply.github.com>
2023-05-02 15:45:40 -0500

    Update prog.py

--- prog.py
+++ prog.py
@@ -4,14 +4,16 @@ import time
 
 ser = serial.Serial('/dev/ttyAMA1', 115200)
 
+
 with open('milestone2.csv', 'r') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     for row in csv_reader:
+        #print(row)
         angle = str(row[0])
         speed = str(row[1])
         duration = str(row[2])
-        command_str = "angle: " + angle + "speed: " + speed + "duration: " + duration
+        command_str = "a:" + angle + " s:" + speed + " d:" + duration + "\n"
         ser.write(bytes(command_str, 'utf-8'))
-        time.sleep(2)
+        time.sleep(int(duration) + 1)
 
 ser.close()
