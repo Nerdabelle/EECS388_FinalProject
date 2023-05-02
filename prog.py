f08b9c2aee4a1ec20b067bb3fb5ea0cf79565be0
jsingh2412 <60803017+jsingh2412@users.noreply.github.com>
2023-05-02 14:27:20 -0500

    Update prog.py
    removed timeout, sleep(2)

--- prog.py
+++ prog.py
@@ -2,7 +2,7 @@ import serial
 import csv
 import time
 
-ser = serial.Serial('/dev/ttyAMA1', 115200, timeout = 1)
+ser = serial.Serial('/dev/ttyAMA1', 115200)
 
 with open('milestone2.csv', 'r') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
@@ -12,6 +12,6 @@ with open('milestone2.csv', 'r') as csv_file:
         duration = str(row[2])
         command_str = "angle: " + angle + "speed: " + speed + "duration: " + duration
         ser.write(bytes(command_str, 'utf-8'))
-        time.sleep(1)
+        time.sleep(2)
 
 ser.close()
